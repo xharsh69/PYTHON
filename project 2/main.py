195e59551655bf117f5986f2452d548873700741
xharsh69 <harshrajsinghx@gmail.com>
2026-06-06 13:24:43 +0530

    create new project(number gussing game)

---
+++ project 2/main.py
@@ -0,0 +1,21 @@
+import random
+
+computer = random.randint(1, 99)
+
+attempts = 1
+
+while True:
+    guess = int(input("Enter your number (1 to 99): "))
+
+    if guess == computer:
+        print(f"🎉 You guessed the right number: {computer}")
+        print(f"Number of attempts: {attempts}")
+        break
+
+    elif guess < computer:
+        print("⬆️ Please enter a higher number.")
+
+    else:
+        print("⬇️ Please enter a lower number.")
+
+    attempts += 1
